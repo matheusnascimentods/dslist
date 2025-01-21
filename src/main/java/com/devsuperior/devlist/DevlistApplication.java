5113aae6b21c183d0f930b711b49a5239c8cfcc2
Matheuz Nascimento <matheusnascimentods@outlook.com>
2025-01-20 21:01:32 -0300

    first commit

---
+++ src/main/java/com/devsuperior/devlist/DevlistApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.devlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevlistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevlistApplication.class, args);
+	}
+
+}
