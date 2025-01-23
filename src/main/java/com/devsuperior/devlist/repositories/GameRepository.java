d6b9d3b514a432fa94bf5fc420fe475b49faef25
Matheuz Nascimento <matheusnascimentods@outlook.com>
2025-01-22 21:22:04 -0300

    Alterações para usar o PostgressSQL

--- src/main/java/com/devsuperior/devlist/repositories/GameRepository.java
+++ src/main/java/com/devsuperior/devlist/repositories/GameRepository.java
@@ -10,7 +10,7 @@ import com.devsuperior.devlist.projection.GameMinProjection;
 
 public interface GameRepository extends JpaRepository<Game, Long>{
 	@Query(nativeQuery = true, value = """
-			SELECT games.id, games.title, games.game_year AS `year`, games.img_url AS imgUrl,
+			SELECT games.id, games.title, games.game_year AS gameYear, games.img_url AS imgUrl,
 			games.short_description AS shortDescription, tb_belonging.position
 			FROM games
 			INNER JOIN tb_belonging ON games.id = tb_belonging.game_id
